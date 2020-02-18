c8314c811bf8fdfd2d9097c3b4ab489f8fba9728
Arul Venkatesh <arulvenkatesh@gmail.com>
2020-02-19 10:01:44 +1100

    redundant imports removed

--- src/test/java/teamcity/live/reporting/RunCucumberTest.java
+++ src/test/java/teamcity/live/reporting/RunCucumberTest.java
@@ -1,14 +1,13 @@
 package teamcity.live.reporting;
 
-import io.cucumber.junit.CucumberOptions;
-import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-import org.junit.runner.RunWith;
+import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"teamcity",
+        plugin = {
+                "teamcity",
                 "pretty",
                 "json:target/report.json",
                 "junit:target/cucumber-reports/Cucumber.xml",
