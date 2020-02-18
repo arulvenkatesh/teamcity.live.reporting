8316d2b1bbd20c5f24ccd3de8102333babc7099f
Arul Venkatesh <arulvenkatesh@gmail.com>
2020-02-19 10:18:09 +1100

    Removed RunWith

--- src/test/java/teamcity/live/reporting/RunCucumberTest.java
+++ src/test/java/teamcity/live/reporting/RunCucumberTest.java
@@ -4,7 +4,6 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-@RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
                 "teamcity",
