68b8be771adb758a8660ae393ac8fa7ca7dcbd1e
Arul Venkatesh <arulvenkatesh@gmail.com>
2020-02-19 11:10:55 +1100

    Commented teamcity

--- src/test/java/teamcity/live/reporting/RunCucumberTest.java
+++ src/test/java/teamcity/live/reporting/RunCucumberTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         plugin = {
-                "teamcity",
+                //"teamcity",
                 "pretty",
                 "json:target/report.json",
                 "junit:target/cucumber-reports/Cucumber.xml",
