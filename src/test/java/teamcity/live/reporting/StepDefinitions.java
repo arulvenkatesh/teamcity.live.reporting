d8c76091cbe52aeffdb61fead9133c608398f611
Arul Venkatesh <arulvenkatesh@gmail.com>
2020-02-19 11:13:53 +1100

    Added teamcity messages

--- src/test/java/teamcity/live/reporting/StepDefinitions.java
+++ src/test/java/teamcity/live/reporting/StepDefinitions.java
@@ -42,5 +42,14 @@ public class StepDefinitions {
     public void fourth_step_is_executed() {
         System.out.println("Executing fourth_step_is_executed started");
         System.out.println("Executing fourth_step_is_executed finished");
+        System.out.println("##teamcity[testSuiteStarted name='suiteName']")
+        System.out.println("##teamcity[testSuiteStarted name='nestedSuiteName']")
+        System.out.println("##teamcity[testStarted name='package_or_namespace.ClassName.TestName']")
+        System.out.println("##teamcity[testFailed name='package_or_namespace.ClassName.TestName' message='The number must be 20000' details='junit.framework.AssertionFailedError: expected:<20000> but was:<10000>|n|r    at junit.framework.Assert.fail(Assert.java:47)|n|r    at junit.framework.Assert.failNotEquals(Assert.java:280)|n|r...']")
+        System.out.println("##teamcity[testFinished name='package_or_namespace.ClassName.TestName']")
+        System.out.println("##teamcity[testStarted name='package_or_namespace.ClassName.TestName2']")
+        System.out.println("##teamcity[testFinished name='package_or_namespace.ClassName.TestName2']")
+        System.out.println("##teamcity[testSuiteFinished name='nestedSuiteName']")
+        System.out.println("##teamcity[testSuiteFinished name='suiteName']")
     }
 }
