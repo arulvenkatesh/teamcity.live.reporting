dd148e286454ab2cbb90c9dafeba5529988bcd9a
Arul Venkatesh <arulvenkatesh@gmail.com>
2020-02-24 23:05:49 +1100

    added testng listener
    Signed-off-by: Arul Venkatesh <arulvenkatesh@gmail.com>

--- src/test/java/teamcity/live/reporting/RunCucumberTest.java
+++ src/test/java/teamcity/live/reporting/RunCucumberTest.java
@@ -2,6 +2,8 @@ package teamcity.live.reporting;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
@@ -18,5 +20,17 @@ public class RunCucumberTest extends AbstractTestNGCucumberTests {
         @DataProvider(parallel = true)
         public Object[][] scenarios() {
                 return super.scenarios();
+        }
+
+        @BeforeClass
+        public void BeforeClass()
+        {
+                System.out.println(String.format("##teamcity[testSuiteStarted name='%s']", "teamcity.live.reporting"));
+        }
+
+        @AfterClass
+        public void AfterClass()
+        {
+                System.out.println(String.format("##teamcity[testSuiteFinished name='%s']", "teamcity.live.reporting"));
         }
 }
---
+++ src/test/java/teamcity/live/reporting/TestngPlugin.java
@@ -0,0 +1,54 @@
+package teamcity.live.reporting;
+
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestngPlugin implements ITestListener {
+
+    @Override
+    public void onFinish(ITestContext arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStart(ITestContext arg0) {
+        System.out.println(String.format("##teamcity[testStarted name='%s' captureStandardOutput='true']", arg0.getName()));
+
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onTestFailure(ITestResult arg0) {
+        // TODO Auto-generated method stub
+        System.out.println(String.format("##teamcity[testFailed type='comparisonFailure' name='%s' message='failure message' details='message and stack trace' expected='expected value' actual='actual value']",arg0.getName()));
+
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onTestStart(ITestResult arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult arg0) {
+        // TODO Auto-generated method stub
+        System.out.println(String.format("##teamcity[testFinished name='%s' duration='50']", arg0.getName()));
+
+
+    }
+}
